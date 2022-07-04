9f8e7429d248fd2ff7f771afabca7ae5c127a332
2nd-update <ahmedellaban1>
2022-07-04 16:41:21 +0100

    models

--- job/models.py
+++ job/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
+JOB = (
+    ('Full time', 'Full time'),
+    ('Part time', 'Part time'),
+)
+
 
 # Create your models here.
 class Job(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, choices=JOB)
+    # location
+
 
 
