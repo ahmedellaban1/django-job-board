f4a561854c9abbdafd2f029641dbb5a66c8c4903
2nd-update <ahmedellaban1>
2022-07-04 03:16:45 +0100

    first commit

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my apps
+    'job',
+    'home',
+    'blog',
+    'contact',
+    'accounts',
 ]
 
 MIDDLEWARE = [
